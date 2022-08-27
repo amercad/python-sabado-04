0ae5f31d4da567fe77677d751d2794dac8989f7c
209 <209@academicos.cesde.edu.co>
2022-08-27 13:19:16 -0500

    NTS4-Listas en python

--- ciclo-for.py
+++ ciclo-for.py
@@ -2,3 +2,8 @@ dato = int( input('Digite un numero: ') )
 
 for obserbador in range(dato):
     print( obserbador )
+    
+nombres=['Andrés', 'Luisa', 'Julian', 'Lina', 'Judit']
+
+for nombre in nombres:
+    print( nombre )
